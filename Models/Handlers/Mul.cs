eddcb571089e4eec03da6524590ace87039ddf05
NSDCode <47573987+NSDCode@users.noreply.github.com>
2022-04-23 03:21:15 +0200

    initial commit

---
+++ Models/Handlers/Mul.cs
@@ -0,0 +1,20 @@
+﻿using dnlib.DotNet.Emit;
+
+
+namespace ArEmulator.Models.Handlers
+{
+    public class Mul : Handler
+    {
+        public override OpCode Type => OpCodes.Mul;
+
+        public override void Handle(ARVM VM, ARVMInstruction instruction)
+        {
+            object a = VM.vmStack.Pop();
+            object b = VM.vmStack.Pop();
+
+            VM.vmStack.Push((int)b * (int)a);
+
+            VM.ind++;
+        }
+    }
+}
---
+++ Models/Handlers/Sub.cs
@@ -0,0 +1,20 @@
+﻿using dnlib.DotNet.Emit;
+
+
+namespace ArEmulator.Models.Handlers
+{
+    public class Sub : Handler
+    {
+        public override OpCode Type => OpCodes.Sub;
+
+        public override void Handle(ARVM VM, ARVMInstruction instruction)
+        {
+            object a = VM.vmStack.Pop();
+            object b = VM.vmStack.Pop();
+
+            VM.vmStack.Push((int)b - (int)a);
+
+            VM.ind++;
+        }
+    }
+}
